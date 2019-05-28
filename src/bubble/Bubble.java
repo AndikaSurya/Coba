afd399d651a5f268ac272662c539732546d044e2
AndikaSurya <SISTEM@DESKTOP-KH5T5LJ>
2019-05-28 21:46:03 +0700

    engkok link frame aku ae

--- src/bubble/Bubble.java
+++ src/bubble/Bubble.java
@@ -5,7 +5,7 @@ public class Bubble {
    static void bubbleSort(int[]arr){
        int n = arr.length;
        int temp = 0;
-       System.out.println("cok");
+       System.out.println("cokk");
        
        for(int i=0;i<n;i++){
            for(int j=1;j<(n-i);j++){
