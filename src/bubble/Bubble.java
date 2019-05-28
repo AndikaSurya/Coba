25f1f53cab6bf9b1bdefc40f09fcb68c5785b4a5
AndikaSurya <SISTEM@DESKTOP-KH5T5LJ>
2019-05-28 21:41:20 +0700

    engkok link frame aku ae

---
+++ src/bubble/Bubble.java
@@ -0,0 +1,38 @@
+package bubble;
+
+public class Bubble {
+    //sort itu artinya mengurutkan
+   static void bubbleSort(int[]arr){
+       int n = arr.length;
+       int temp = 0;
+       System.out.println("cok");
+       
+       for(int i=0;i<n;i++){
+           for(int j=1;j<(n-i);j++){
+               if(arr[j-1]>arr[j]){
+                   temp = arr[j-1];//z=x
+                   arr[j-1] = arr[j];//x=y
+                   arr[j] = temp;//y=z
+               }
+               
+           }
+       }
+   }
+
+   public static void main(String[]args){
+       int arr[]={9,6,3,5,8,7};
+       for(int i=0;i<arr.length;i++){
+           System.out.print(arr[i]+"");
+       }
+       System.out.println();
+       bubbleSort(arr);
+       
+       for(int i=0;i<arr.length;i++){
+           System.out.print(arr[i]+"");}
+   }         
+}  
+    
+ 
+    
+ 
+  
